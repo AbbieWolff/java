966387b5bd7e6d3d0afa035831b4fc460926419c
Abbie Wolff <AbbieWolff@users.noreply.github.com>
2015-07-21 02:14:51 -0400

    Create ShowCommission_GUI.java

---
+++ ShowCommission_GUI.java
@@ -0,0 +1,24 @@
+/*
+ * Abbie Wolff
+ * June 2015
+ * 
+ * Java Programming Week 8 Assignment:
+ * This program calculates commission based on user input in a GUI.
+ * When users input a sales earnings amount and commission rate,
+ * the program 1) calculates their earned commission amount, 
+ * 2) calculates their total income (including commission), and 
+ * 3) displays the results of these calculations in the GUI.
+ */
+
+package commission_gui;
+
+public class ShowCommission_GUI {
+    
+    // Command line arguments.
+    public static void main(String args[]) {
+        
+        // Initialize GUI.
+        new Commission_GUI();
+        
+    }
+}
