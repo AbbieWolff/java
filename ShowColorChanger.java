d1eff80622ba76ce92e59420f6bfe5f53e61396f
Abbie Wolff <AbbieWolff@users.noreply.github.com>
2015-08-03 23:32:50 -0400

    Create ShowColorChanger.java

---
+++ ShowColorChanger.java
@@ -0,0 +1,18 @@
+/*
+ * Abbie Wolff
+ * August 2015
+ * 
+ * Java Programming Week 10 Assignment:
+ * This program enables users to select the background color of a form with a 
+ * GUI combo box.
+ */
+package colorchanger;
+
+public class ShowColorChanger {
+ // Command line arguments.
+    public static void main(String args[]) {
+        
+        // Initialize GUI.
+        new ColorChanger();
+    }   
+}
