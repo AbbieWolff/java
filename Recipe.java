3a247cd72683615b369944481ddb2a11afb7d5b9
Abbie Wolff <AbbieWolff@users.noreply.github.com>
2015-08-09 04:01:36 -0400

    Create Recipe.java

---
+++ Recipe.java
@@ -0,0 +1,40 @@
+/*
+ * Abbie Wolff
+ * August 2015
+ * 
+ * Java Programming Final Project:
+ * A program that aids players of The Witcher 3: Wild Hunt video game
+ * with inventory management and trading in-game. It is not a mod.
+ * Users enter the ingredient they are wondering whether they should keep
+ * or sell, and the program returns with an answer, based on a database of
+ * information about what alchemy & crafting recipes require each ingredient.
+ * Users also keep the app up to date on which recipes they have created so far
+ * so that the app can tell them an up-to-date grocery list of needed and
+ * not-needed recipe ingredients.
+ */
+
+package inventorymanagement;
+
+// imports 
+
+public class Recipe {
+    
+    public Recipe myRecipe;
+    
+    // Declare a name string with set & get methods.
+    String name;
+        public void setName(String name) {
+            // insert code
+        }
+        public void getName(String name) {
+        }
+
+    // Declare a description string with set & get methods.
+    String description;
+        public void setDescription(String description) {
+            // insert code
+        }
+        public void getDescription(String description) {
+        }
+ 
+}
