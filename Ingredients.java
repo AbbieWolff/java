38f745cf4d4948219322a39b4a9ff4a20b26aca3
Abbie Wolff <AbbieWolff@users.noreply.github.com>
2015-08-09 04:01:52 -0400

    Create Ingredients.java

---
+++ Ingredients.java
@@ -0,0 +1,76 @@
+/*
+ * Abbie Wolff
+ * August 2015
+ * 
+ * Java Programming Final Project:
+ * A program that aids players of The Witcher 3: Wild Hunt video game
+ * with inventory management and trading in-game. It is not a mod.
+ * Users enter the ingredient they are wondering whether they should keep
+ * or sell, and the program returns with an answer, based on a database of
+ * information about what alchemy & crafting recipes require each ingredient.
+ * Users also keep the app up to date on which recipes they have created so far
+ * so that the app can tell them an up-to-date grocery list of needed and
+ * not-needed recipe ingredients.
+ */
+
+package inventorymanagement;
+
+// imports
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+
+class Ingredients {
+    
+    public Ingredients myIngredients;
+    
+    // Declare an array list called ingredients.
+    public ArrayList ingredients = new ArrayList();
+    
+    /*      
+    // Add entries to the ingredients array list.
+    ingredients.add("zero");
+    
+    // Set a specific entry of the ingredients array list.
+    ingredients.set(0, "zero");
+    
+    // Get the contents of the ingredients array list at index 0.
+    ingredients.get(0);
+        
+    // Remove an item from an ArrayList.
+    //ingredients.remove(10);
+    //ingredients.remove( "ten");
+    
+    // Declare count integer for info about the size (# entries) in ingredients.
+    int count = ingredients.size();
+    
+    // Increase capacity of the array list to ensure it can hold # of elements.
+    ingredients.ensureCapacity(int minCapacity);
+        
+    // Sort ingredients alphabetically
+    Collections.sort(ingredients); // sort it if it's a collection
+    Arrays.sort(ingredients); // sort it if it's an array
+    
+    // Initialize an iterator for the ingredients array list.
+    Iterator it = ingredients.iterator();
+    
+    // Utilize the iterator to print all items in the list.
+    while(it.hasNext()) {
+        System.out.println(it.next());
+    }
+    
+    // Print the contents of the array list.
+    for (String value : ingredients) {
+        System.out.println(value);
+    } 
+        
+    // Print the entire list.
+    System.out.println("Ingredients: " + ingredients);
+    
+    // Create a new array containing all the elements in an object array in seq.
+    public Object[] toArray[];
+    */
+}
