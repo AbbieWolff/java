706b262f944a109e47ecc87bce175cb3783c179d
Abbie Wolff <AbbieWolff@users.noreply.github.com>
2015-08-17 19:48:31 -0400

    Create RecipeNames.java

---
+++ RecipeNames.java
@@ -0,0 +1,117 @@
+Aether
+Albedo
+Alcohest
+Alghoul Decoction
+Ancient Leshen Decoction
+Arachas Decoction
+Archgriffin Decoction
+Basilisk Decoction
+Bear Pheromones
+Beast Oil
+Black Blood
+Blizzard
+Cat
+Chort Decoction
+Cockatrice Decoction
+Cursed Oil
+Doppler Decoction
+Draconid Oil
+Drowner Pheromones
+Dwarven Spirit
+Earth Elemental Decoction
+Ekhidna Decoction
+Ekimmara Decoction
+Elementa Oil
+Enhanced Beast Oil
+Enhanced Black Blood
+Enhanced Blizzard
+Enhanced Cat
+Enhanced Cursed Oil
+Enhanced Draconid Oil
+Enhanced Elementa Oil
+Enhanced Full Moon
+Enhanced Golden Oriole
+Enhanced Hanged Man's Venom
+Enhanced Hybrid Oil
+Enhanced Insectoid Oil
+Enhanced Maribor Forest
+Enhanced Necrophage Oil
+Enhanced Ogroid Oil
+Enhanced Petri's Philter
+Enhanced Relict Oil
+Enhanced Specter Oil
+Enhanced Swallow
+Enhanced Tawny Owl
+Enhanced Thunderbolt
+Enhanced Vampire Oil
+Enhanced White Honey
+Enhanced White Rafford's Decoction
+Fiend Decoction
+Foglet Decoction
+Forktail Decoction
+Full Moon
+Golden Oriole
+Grave Hag Decoction
+Griffin Decoction
+Hanged Man's Venom
+Hybrid Oil
+Hydragenum
+Insectoid Oil
+Katakan Decoction
+Killer Whale
+Leshen Decoction
+Maribor Forest
+Necrophage Oil
+Nekker Pheromones
+Nekker Warrior Decoction
+Nightwraith Decoction
+Nigredo
+Noonwraith Decoction
+Ogroid Oil
+Petri's Philter
+Pop's Mold Antidote
+Quebrith
+Rebis
+Relict Oil
+Reliever's Decoction
+Rubedo
+Specter Oil
+Succubus Decoction
+Superior Beast Oil
+Superior Black Blood
+Superior Blizzard
+Superior Cat
+Superior Cursed Oil
+Superior Draconid Oil
+Superior Elementa Oil
+Superior Full Moon
+Superior Golden Oriole
+Superior Hanged Man's Venom
+Superior Hybrid Oil
+Superior Insectoid Oil
+Superior Maribor Forest
+Superior Necrophage Oil
+Superior Ogroid Oil
+Superior Petri's Philter
+Superior Relict Oil
+Superior Specter Oil
+Superior Swallow
+Superior Tawny Owl
+Superior Thunderbolt
+Superior Vampire Oil
+Superior White Honey
+Superior White Rafford's Decoction
+Swallow
+Tawny Owl
+Thunderbolt
+Troll Decoction
+Vampire Oil
+Vermilion
+Vitriol
+Water Hag Decoction
+Werewolf Decoction
+White Gull
+White Honey
+White Raffard's Decoction
+Wraith Decoction
+Wyvern Decoction
